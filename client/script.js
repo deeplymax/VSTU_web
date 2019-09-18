c4db720c9cd24064ee3e56f98a123a7d6a57402b
deeplymax <deeplymax@gmail.com>
2019-09-18 15:13:39 +0400

    Connected javascript, getted description value

---
+++ client/script.js
@@ -0,0 +1,7 @@
+//When window load
+document.addEventListener('DOMContentLoaded', function() {
+    //Get description from textarea
+    let description = document.querySelector('#description');
+    console.log(description.value);
+});
+
