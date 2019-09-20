7307d3195aafd6e5cdcb7fe37a71f2cc0d8cb76d
deeplymax <deeplymax@gmail.com>
2019-09-20 15:10:16 +0400

    Testing event listener

--- client/script.js
+++ client/script.js
@@ -1,7 +1,13 @@
 //When window load
-document.addEventListener('DOMContentLoaded', function() {
-    //Get description from textarea
-    let description = document.querySelector('#description');
-    console.log(description.value);
+document.addEventListener('DOMContentLoaded', () => {
+    let regexp = document.querySelector('#regexp');
+    
+    regexp.addEventListener('input', (event) => {
+        console.log(event.target.value);
+    })
+
+    document.querySelector('.samples').oninput = event => {
+        console.log(event.target.value);        
+    }
 });
 
