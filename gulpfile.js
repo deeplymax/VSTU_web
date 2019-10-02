6c5eaec485b3e84894576782eef66d7217a306e9
deeplymax <deeplymax@gmail.com>
2019-10-02 11:16:11 +0400

    testing gulp task for scss

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('build-css', () => {
+    return gulp.src('./client/src/test_sass.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./clint/dest'))
+})
+
