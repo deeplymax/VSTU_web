1af95da9fbfa475915b249d1aa0e4bd27174875a
deeplymax <deeplymax@gmail.com>
2019-10-02 13:07:21 +0400

    deleted tests and created style.scss

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
 gulp.task('build-css', () => {
-    return gulp.src('./client/src/test_sass.scss', { sourcemaps:true })
+    return gulp.src('./client/src/style.scss', { sourcemaps:true })
     .pipe(sass())
     .pipe(gulp.dest('./client/dest',{ sourcemaps:true }))
 });
 
 gulp.task('default', gulp.series('build-css', () => {
-    gulp.watch('./client/src/test_sass.scss', 
+    gulp.watch('./client/src/style.scss', 
     gulp.series('build-css'));
 })); 
