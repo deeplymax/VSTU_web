3e515bdfcb2ce7256b087a9a58a03a894e639bc1
deeplymax <deeplymax@gmail.com>
2019-11-01 12:39:25 +0400

    started server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Lets start");
+const http = require('http');
+const url = require('url');
+const server = http.createServer((request, response) => {
+    const parsedUrl = url.parse(request.url, true);
+    response.write(JSON.stringify(parsedUrl));
+    response.end();
+});
+const port = 3000;
+
+server.listen(port, () => {
+    console.log("Server started at http://localhost:" + port);
+})
